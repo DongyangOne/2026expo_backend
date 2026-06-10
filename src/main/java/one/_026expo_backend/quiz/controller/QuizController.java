b6dbbbfda9f67693000612edc31590e5f9962dc4
marianddi <hanna4331@naver.com>
2026-06-11 07:02:14 +0900

    docs : 스웨거 문서 오타 수정

--- src/main/java/one/_026expo_backend/quiz/controller/QuizController.java
+++ src/main/java/one/_026expo_backend/quiz/controller/QuizController.java
@@ -1,13 +1,8 @@
 package one._026expo_backend.quiz.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import one._026expo_backend.global.config.swagger.ApiErrorExceptions;
 import one._026expo_backend.global.dto.ApiResponse;
@@ -35,7 +30,7 @@ public class QuizController {
      * 응답 데이터 : 퀴즈 리스트를 저장하는 redis session id, 첫번째 퀴즈 정보(id, 문제)
      */
     @ApiErrorExceptions({ErrorCode.QUIZ_NOT_FOUND, ErrorCode.NOT_ENOUGH_QUIZ, ErrorCode.USER_NOT_FOUND})
-    @Operation(summary = "퀴즈 시작", description = "유저가 선택한 퀴즈 개수를 이용하여 퀴즈 id리스트와 첫번재 문제 정보를 불러옵니다.")
+    @Operation(summary = "퀴즈 시작", description = "유저가 선택한 퀴즈 개수를 이용하여 퀴즈 id리스트와 첫번째 문제 정보를 불러옵니다.")
     @PostMapping("/start")
     public ResponseEntity<ApiResponse<StartQuizResponseDto>> StartQuiz(
             @AuthenticationPrincipal Long userId,
