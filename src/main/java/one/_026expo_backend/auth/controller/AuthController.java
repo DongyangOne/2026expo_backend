ae86018f06767f6625601c09f3a73eeba99610ad
LivingJisu <reply04@dongyang.ac.kr>
2026-06-11 19:11:34 +0900

    fix: AuthController 불필요한 import문 삭제

--- src/main/java/one/_026expo_backend/auth/controller/AuthController.java
+++ src/main/java/one/_026expo_backend/auth/controller/AuthController.java
@@ -32,7 +32,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
