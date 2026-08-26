d5c0141e1655e76d5bd4e7eee00f524bb8638b5a
marianddi <hanna4331@naver.com>
2026-08-26 18:34:28 +0900

    refactor : 코드래빗 리뷰반영

--- src/main/java/one/_026expo_backend/global/pagination/PageRequestDto.java
+++ src/main/java/one/_026expo_backend/global/pagination/PageRequestDto.java
@@ -16,10 +16,12 @@ import lombok.Setter;
 public class PageRequestDto {
 
     @Schema(example = "0", description = "페이지 번호 (기본값: 0)")
+    @NotNull(message = "페이지 번호를 입력해주세요.")
     @Min(value = 0, message = "페이지 번호는 0 이상이어야 합니다.")
     private Integer page = 0;
 
     @Schema(example ="10", description = "페이지 크기 (기본값: 10)")
+    @NotNull(message = "페이지 크기를 입력해주세요.")
     @Min(value = 1, message = "페이지 크기는 1 이상이어야 합니다.")
     @Max(value = 100, message = "페이지 크기는 100 이하이어야 합니다.")
     private Integer pageSize = 10;
