f7520918b08abe3659cc9c77d354b3a1edf9670a
marianddi <hanna4331@naver.com>
2026-08-26 17:42:51 +0900

    refactor : 피드백 리스트 조회 오류 해결

--- src/main/java/one/_026expo_backend/global/pagination/PageRequestDto.java
+++ src/main/java/one/_026expo_backend/global/pagination/PageRequestDto.java
@@ -3,22 +3,25 @@ package one._026expo_backend.global.pagination;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class PageRequestDto {
 
-    @Schema(example = "0", description = "페이지 번호")
-    @Min(0)
+    @Schema(example = "0", description = "페이지 번호 (기본값: 0)")
+    @Min(value = 0, message = "페이지 번호는 0 이상이어야 합니다.")
     private Integer page = 0;
 
-    @Schema(example ="10", description = "페이지 크기")
-    @Min(1)
-    @Max(100)
+    @Schema(example ="10", description = "페이지 크기 (기본값: 10)")
+    @Min(value = 1, message = "페이지 크기는 1 이상이어야 합니다.")
+    @Max(value = 100, message = "페이지 크기는 100 이하이어야 합니다.")
     private Integer pageSize = 10;
 
     @Schema(hidden = true)
